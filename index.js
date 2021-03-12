46034b39612ff01faa302dd32567c3ad8f174065
Patricia Molnar <m.patricia@live.com>
2021-03-12 18:36:23 +0000

    backend: formatting API response data

--- index.js
+++ index.js
@@ -7,33 +7,39 @@ app.listen(3000, () => console.log('listening at port 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-const formatData = data => {
-    const object = {
-        amount: data.element_count 
+const formatData = (data) => {
+    const arr = data.near_earth_objects[Object.keys(data.near_earth_objects)[0]];
+    const asteroids = arr.map(item => {
+        const obj = {
+            name: item.name, 
+            jpl_url: item.nasa_jpl_url, 
+            abs_magnitude: item.absolute_magnitude_h,
+            diameter: item.estimated_diameter, 
+            hazard: item.is_potentially_hazardous_asteroid,
+            approach: item.close_approach_data
+        }
+        return obj;
+    })
+
+    const result = {
+        amount: data.element_count,
+        asteroids: asteroids 
     }
 
-    return object;
+    return result;
 }
 
 //route for post request
 app.get('/api/:date', (req, res) => {
-    //get today
     const q = req.params.date;
-    console.log(q);
-
-    //build URL
     const api_key = process.env.API_KEY;   
     const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${q}&end_date=${q}&api_key=${api_key}`;
 
-    //fetch data and return it 
-    // fetch(url)
-    //   .then(res => res.json())
-    //   .then(json => formatData(json))
-    //   .then(data => res.json(data)); 
-
+    //fetch data and send to frontend 
     fetch(url)
       .then(res => res.json())
-      .then(json => res.json(json));  
+      .then(json => formatData(json))
+      .then(data => res.json(data)); 
 });
 
 
