122d7cddccaeaf266b3affdd6cd5365e3c495687
Patricia Molnar <m.patricia@live.com>
2021-03-16 18:22:57 +0000

    added option for user to change metric unit from km to m and vice versa

--- public/script.js
+++ public/script.js
@@ -6,13 +6,19 @@ window.addEventListener('DOMContentLoaded', function() {
     const neoFastest = document.getElementById('neo-fastest');
     const neoClosest = document.getElementById('neo-closest'); 
 
-    const appendData = (json) => {
+    const appendData = (json, metric) => {
         console.log(json); 
         neoTotal.textContent = json.amount;
-        filterSize(json.asteroids);
+
+        if(metric === 'km') {
+            filterSizeKm(json.asteroids);
+        } else {
+            filterSizeM(json.asteroids);
+        }
+        
     }
 
-    const filterSize = (arr) => {
+    const filterSizeKm = (arr) => {
         let countHazard = 0; 
         let biggest = [0, 0], fastest = 0, closest = 0; 
         let nameBiggest = '', nameFastest = '', nameClosest = '';
@@ -47,18 +53,68 @@ window.addEventListener('DOMContentLoaded', function() {
         }); 
 
         //if diameter smaller than a kilometer => convert to meters.
-        let metric = 'kilometers';
+        let txt = 'kilometers';
         if(biggest[0] < 1) {
             biggest[0] *= 1000;
             biggest[1] *= 1000;
-            metric = 'meters';
+            txt = 'meters';
         } 
 
         //append to DOM
         neoHazard.textContent = countHazard; 
-        neoBiggest.textContent = `${Number(biggest[0].toFixed(2)).toLocaleString()} - ${Number(biggest[1].toFixed(2)).toLocaleString()} ${metric}`;
+        neoBiggest.textContent = `${Number(biggest[0].toFixed(2)).toLocaleString()} - ${Number(biggest[1].toFixed(2)).toLocaleString()} ${txt}`;
         neoFastest.textContent = `${Number(fastest.toFixed(2)).toLocaleString()}km/h`;
         neoClosest.textContent = `${Number(closest.toFixed(2)).toLocaleString()}km away from Earth`;  
+    }
+
+    //get information in miles 
+    const filterSizeM = (arr) => {
+        let countHazard = 0; 
+        let biggest = [0, 0], fastest = 0, closest = 0; 
+        let nameBiggest = '', nameFastest = '', nameClosest = '';
+
+        arr.forEach(neo => {
+            //count hazardous NEOs
+            if(neo.hazard == true) {
+                ++countHazard;
+            }
+
+            //save biggest NEO
+            const diameterMin = parseFloat(neo.diameter.miles.estimated_diameter_min);
+            const diameterMax = parseFloat(neo.diameter.miles.estimated_diameter_max);
+            
+            if(diameterMin > biggest[0]) {
+                biggest[0] = diameterMin;
+                biggest[1] = diameterMax;
+                nameBiggest = neo.name;
+            }
+            //save fastest NEO
+            const velocity = parseFloat(neo.approach[0].relative_velocity.miles_per_hour)
+            if(velocity > fastest) {
+                fastest = velocity;
+                nameFastest = neo.name;
+            }
+            //save closest NEO
+            const distance = parseFloat(neo.approach[0].miss_distance.miles);
+            if(distance > closest) {
+                closest = distance;
+                nameClosest = neo.name;
+            }
+        }); 
+
+        //if diameter smaller than a kilometer => convert to meters.
+        let txt = 'miles';
+        if(biggest[0] < 1) {
+            biggest[0] *= 5280;
+            biggest[1] *= 5280;
+            txt = 'feet';
+        } 
+
+        //append to DOM
+        neoHazard.textContent = countHazard; 
+        neoBiggest.textContent = `${Number(biggest[0].toFixed(2)).toLocaleString()} - ${Number(biggest[1].toFixed(2)).toLocaleString()} ${txt}`;
+        neoFastest.textContent = `${Number(fastest.toFixed(2)).toLocaleString()}m/h`;
+        neoClosest.textContent = `${Number(closest.toFixed(2)).toLocaleString()}m away from Earth`;  
     }
 
     //create date based on milliseconds
@@ -71,35 +127,33 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     //query API directly with date string
-    const fetchApi = (query) => {
+    const fetchApi = (query, metric) => {
         //call API with specified query
         fetch('/api/' + query)
             .then(response => response.json())
-            .then(data => appendData(data));
+            .then(data => appendData(data, metric));
 
         //append date to DOM
         dateSpan.textContent = query;
     }
 
     // when page loads query database with today's date. 
-    let today = createDate(Date.now());
-    fetchApi(today);
-
-    //variable to keep track of how many days we are away from today.
-    let num = 0; 
+    let current = Date.now();
+    let metric = 'km'
+    fetchApi(createDate(current), metric); 
 
     //load next day 
     const next = document.getElementById('next'); 
     next.addEventListener('click', () => {
-        ++num;
-        fetchApi(createDate(Date.now() + 8.64e+7 * num)); 
+        current += 8.64e+7;
+        fetchApi(createDate(current), metric); 
     });
 
     //load previous day
     const prev = document.getElementById('prev'); 
     prev.addEventListener('click', () => {
-        --num;
-        fetchApi(createDate(Date.now() + 8.64e+7 * num));
+        current -= 8.64e+7;
+        fetchApi(createDate(current), metric);
     });
 
      
@@ -127,7 +181,15 @@ window.addEventListener('DOMContentLoaded', function() {
 
     const searchBtn = document.getElementById('search-btn'); 
     searchBtn.addEventListener('click', function() {
-        fetchApi(`${year.value}-${month.value}-${day.value}`);
+        let input = `${year.value}-${month.value}-${day.value}`;
+        current = new Date(year.value, month.value - 1, day.value).getTime();
+        fetchApi(input, metric);
     }); 
     
+    //let user change the preferred metric unit 
+    const metricSelect = document.getElementById('metric'); 
+    metricSelect.addEventListener('change', () => {
+        metric = metricSelect.value; 
+        fetchApi(createDate(current), metric);
+    });
 });
