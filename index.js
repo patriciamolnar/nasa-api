5ece04df1b5618f8ce30a32e4d0db7b96d3e4a2d
Patricia Molnar <m.patricia@live.com>
2021-04-09 18:29:42 +0100

    changing port from hardcoded 3000 to environmental var

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const fetch = require('node-fetch');
 require('dotenv').config(); 
 
 const app = express();
-app.listen(3000, () => console.log('listening at port 3000'));
+const port = process.env.PORT;
+app.listen(port, () => console.log(`listening at port ${port}`));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
